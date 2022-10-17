ac92dd7e7512447f11020e9c38eab27949356373
Bernward Sanchez <109844019+pneb@users.noreply.github.com>
2022-10-17 18:00:08 +0800

    Use multiprocess
    yes

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import core
 import threading
 import time
+import core
+import os
+import requests
+from multiprocessing import Pool, current_process
 
-THREADS = 500
+THREADS = 50
 TIMEOUT = 5
-
+PROXIES_FILE = "proxies.txt"
+VALID_FILE = "multiprocessing_valid.txt"
+VALID_PROXIES = []
 
 def loadProxies():
-    with open("proxies.txt") as file:
+    lines = []
+    with open(PROXIES_FILE) as file:
         lines = file.readlines()
-        return [line.rstrip() for line in lines]
+    return [line.rstrip() for line in lines]
 
 def checkProxy(proxy):
     try:
@@ -17,14 +23,24 @@ def checkProxy(proxy):
         valid = checker.check()
         if valid:
             print(f"[VALID] {proxy}")
-            with open("valid.txt", "a") as validFile:
-                validFile.write(f"{proxy}\n")
-
+            VALID_PROXIES.append(proxy)
+        
     except:
         print(f"[ERROR] {proxy}")
 
-proxies = loadProxies()
-for proxy in proxies:
-    threading.Thread(target=checkProxy, args=(proxy,)).start()
-    time.sleep(TIMEOUT/THREADS)
+def writeValid():
+    if len(VALID_PROXIES) > 0:
+        with open(VALID_FILE, "a") as file:
+            for validProxy in VALID_PROXIES:
+                file.write(f'{validProxy}\n')
+            print('[+] Saved valid proxies...')
+        print(f'[+] Done!')
+    else:
+        print('[!] No valid proxies...')
+
+if __name__ == "__main__":    
+    with Pool(THREADS) as pool:
+        arguments = (proxy for proxy in loadProxies())
+        pool.map(checkProxy, arguments)
     
+    writeValid()
